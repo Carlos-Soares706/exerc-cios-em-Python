bb701969305256614175fff6bfccb7908a337431
Gleiciele <chnsoares@outlook.com>
2024-07-18 14:15:56 -0300

    first commit

--- exercicio14.py
+++ exercicio14.py
@@ -5,3 +5,11 @@ Enunciado: Dada uma lista de pares de palavras, escreva um programa que compare 
 
 pares_palavras = [("casa", "laranja"), ("banana", "uva"), ("python", "java"), ("amor", "amizade")]
 """
+pares_palavras = [("casa", "laranja"), ("banana", "uva"), ("python", "java"), ("amor", "amizade")]
+
+for duplas in pares_palavras:
+     
+    if len(duplas[0]) > len(duplas[1]):
+        print(f'{duplas[0]}')
+    if len(duplas[0]) < len(duplas[1]):
+        print(f'{duplas[1]}')
